feb31a33efcfe8009b8d4ee4ba17e842e528d380
aleung30 <aleung30@student.ubc.ca>
2025-09-20 14:31:22 -0700

    implemented convexhull functions

--- convexhull.cpp
+++ convexhull.cpp
@@ -7,17 +7,74 @@
  * @author      CPSC 221: Basic Algorithms and Data Structures
  *
  * THIS FILE MUST BE SUBMITTED
-**/
+ **/
 
 #include "convexhull.h"
+#include <algorithm>
 
 /***************************************************
-* IF YOU WISH TO DEFINE YOUR OWN CUSTOM FUNCTIONS, *
-* ADD THEM HERE                                    *
-***************************************************/
+ * IF YOU WISH TO DEFINE YOUR OWN CUSTOM FUNCTIONS, *
+ * ADD THEM HERE                                    *
+ ***************************************************/
 
+/**
+ *
+ */
 
+bool straight(pair<double, double> piv, pair<double, double> p1, pair<double, double> p2)
+{
+    return (p1.second - piv.second) * (p2.first - p1.first) == (piv.first - p1.first) * (p2.second - p1.second);
+}
 
+bool between(pair<double, double> piv, pair<double, double> p1, pair<double, double> p2)
+{
+    return min(piv.first, p1, first) <= p2.first && p2.first <= max(piv.first, p2.first) &&
+           min(piv.second, p1.second) <= p2.second &&
+           p2.second <= max(piv.second, p1.second);
+}
+
+int distance(pair<double, double> p1, pair<double, double> p2)
+{
+    return (p1.first - p2.first) * (p1.first - p2.first) + (p1.second - p2.second) * (p1.second - p2.second);
+}
+
+int polar(pair<double, double> piv, pair<double, double> p1, pair<double, double> p2)
+{
+    bool a = p1.first - piv.first >= 0;
+    bool b = p2.first - piv.first >= 0;
+    if (a && !b)
+    {
+        return -1;
+    }
+    if (!a && b)
+    {
+        return 1;
+    }
+
+    if (CCW(piv, p1, p2))
+    {
+        return -1;
+    }
+    if (CCW(piv, p2, p1))
+    {
+        return 1;
+    }
+    int distance1 = distance(piv, p1);
+    int distance2 = distance(piv, p2);
+
+    if (distance1 < distance2)
+    {
+        return -1;
+    }
+    else if (distance1 > distance2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 /**
  * Uses a sorting algorithm of your choice(must be fully implemented by you)
  * to sort the points in v to satisfy the following criteria:
@@ -29,18 +86,31 @@
  * 	one of the legs of the angle is represened by the line through v[0] and
  *	v[i], and the other is the x-axis.
  * NOTE: "smallest" y-coordinate is actually closest to the TOP of a PNG image.
-**/
-void SortByAngle(vector<pair<double, double>>& v) {
-	/* Add your code below */
-	
+ **/
+void SortByAngle(vector<pair<double, double>> &v)
+{
+    int min = 0;
+    for (int i = 1; i < v.size(); i++)
+    {
+        if (v[i].second < v[min].second || (v[i].second == v[min].second && v[i].first < v[min].first))
+        {
+            min = i;
+        }
+    }
+    swap(v[0], v[min]);
+    pair<double, double> piv = v[0];
+
+    sort(v.begin() + 1, v.end(), [piv](pair<double, double> p1, pair<double, double> p2)
+         { return polar(piv, p1, p2) < 0; });
 }
 
 /**
  * Determines whether a path from p1 to p2 to p3 describes a counterclockwise turn
-**/
-bool CCW(pair<double, double> p1, pair<double, double> p2, pair<double, double> p3) {
-	/* Replace the line below with your code */
-	return false; // REPLACE THIS STUB
+ **/
+bool CCW(pair<double, double> p1, pair<double, double> p2, pair<double, double> p3)
+{
+    int path = (p2.second - p1.second) * (p3.first - p2.first) - (p2.first - p1.first) * (p3.second - p2.second);
+    return path > 0;
 }
 
 /**
@@ -51,13 +121,44 @@ bool CCW(pair<double, double> p1, pair<double, double> p2, pair<double, double> 
  * Note that only the endpoints of a colinear sequence will belong in the hull.
  * The returned vector should be a subset of v
  * Input: v - a vector of points in arbitrary order
-**/
-vector<pair<double, double>> GetConvexHull(vector<pair<double, double>>& v) {
-	vector<pair<double, double>> hull;
+ **/
+vector<pair<double, double>> GetConvexHull(vector<pair<double, double>> &v)
+{
+    vector<pair<double, double>> hull;
+    stack<pair<double, double>> stack;
 
-	/* Add your code below */
+    if (v.size() < 3)
+    {
+        return hull;
+    }
 
+    SortByAngle(v);
+    stack.Push(v[0]);
+    stack.Push(v[1]);
 
+    for (int i = 2; i < v.size(); i++)
+    {
+        while (stack.Size() >= 2)
+        {
+            pair<double, double> a = stack.Peek();
+            stack.Pop();
+            pair<double, double> b = stack.Peek();
 
-	return hull;
+            if (CCW(a, b, v[i]))
+            {
+                stack.Push(b);
+                break;
+            }
+        }
+        stack.Push(v[i]);
+    }
+
+    while (!stack.IsEmpty())
+    {
+
+        hull.push_back(stack.Peek());
+        stack.Pop();
+    }
+    reverse(hull.begin(), hull.end());
+    return hull;
 }
